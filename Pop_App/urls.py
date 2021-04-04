49457af8671edc2812f5725c8a76db0c95d43390
RowdyOnline <rowdy14@gmail.com>
2021-04-03 23:02:39 -0700

    Pre-Log in pages( including urls + views)

--- Pop_App/urls.py
+++ Pop_App/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('userAccess', views.userAccess),
+    path('category/<str:category>', views.categoryPage),
+    path('bookDetails/<int:bookId>', views.bookDetails),
+    path('catalogue', views.catalogue)
 ]
